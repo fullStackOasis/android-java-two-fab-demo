a6795ee1d5ba131884a3cf2891f703eb3772582b
fullStackOasis <fullstackdev@fullstackoasis.com>
2021-12-04 15:59:52 -0500

    Two FABs in place. Use intuit library to easily scale the different FABs on different devices so they don't overlap. Change theme colors to purple.

--- app/src/main/java/com/fullstackoasis/twofabsdemo/MainActivity.java
+++ app/src/main/java/com/fullstackoasis/twofabsdemo/MainActivity.java
@@ -36,7 +36,14 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        binding.fabDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        binding.fabSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
